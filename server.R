4ccefaa38a2719039f748e3e37034f409f0a6166
gabbyyou <qianhuiy@marshall.usc.edu>
2016-12-04 17:16:13 -0800

    Add files via upload

--- server.R
+++ server.R
@@ -4,44 +4,70 @@ library(dplyr)
 
 server = function(input, output) {
   
-  weekdayst = reactive({
-    weekdayst = read.csv("weekdayst.csv")
-    weekdayst$day = factor(weekdayst$day, levels = c("Mon","Tues","Wed","Thurs","Fri","Sat","Sun"))
+ 
+  
+### volume time data 
+  dayhour = reactive({
+    dayhour = read.csv("dayhour.csv")
+  })
+  monthhr = reactive({
+    monthhr = read.csv("monthhr.csv")
   })
   
+### volume time plot
+  ## volume time input
+  volumetimeplot = reactive({
+    if(input$vtselect == "hourday") {
+      ggplot(dayhour(),aes(x=daynum,y=hour,fill=count))+
+        geom_tile()+
+        scale_fill_gradient(low="white",high="black") +
+        ylab("Hour")+
+        xlab("")+
+        scale_x_continuous(breaks = seq(1,7,1),
+                           labels = c("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"))+
+        theme_classic()
+    } else{
+      if(input$vtselect == "hourmonth") {
+        ggplot(monthhr(),aes(x=monnum,y=hour,fill=count))+
+          geom_tile()+
+          xlab("")+
+          ylab("Hour")+
+          scale_x_continuous(breaks = seq(1,12,1),
+                             labels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))+
+          scale_fill_gradient(low="white",high="black")+
+          theme_classic()
+      } else { ### daymonth
+        
+      }}
+  })
+  ##vlume time output
+  output$volumetime = renderPlot({
+    volumetimeplot()
+  })  
+
   output$dayhour <- renderPlot({
-    dayhour$day = factor(dayhour$day, levels = c("Mon","Tues","Wed","Thurs","Fri","Sat","Sun"))
-    ggplot(dayhour,aes(x=day,y=hour,fill=count))+
+    ggplot(dayhour(),aes(x=daynum,y=hour,fill=count))+
       geom_tile()+
       scale_fill_gradient(low="white",high="black") +
       ylab("Hour")+
       xlab("")+
+      scale_x_continuous(breaks = seq(1,7,1),
+        labels = c("Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"))+
       theme_classic()
   })
   
-  #output$requesttime <- renderPlot({
-   # monthst$month = factor(monthst$month, levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
-    #ggplot(monthst,aes(x=factor(month),y=RequestType,fill=count))+
-     # geom_tile()+
-      #scale_fill_gradient(low="white",high="black")+
-      #theme_classic()+
-      #xlab("") +
-      #ylab("")+
-      #theme_classic()
-  #  })
-    
- 
-    monthoutput = eventReactive(input$monthclick, {
-      ggplot(monthst,aes(x=factor(month),y=RequestType,fill=count))+
-        geom_tile()+
-        scale_fill_gradient(low="white",high="black")+
-        theme_classic()+
-        xlab("") +
-        ylab("")+
-        theme_classic()
-    })
-    
-    houroutput = eventReactive(input$hourclick, {
+###request time plot   
+  requesttimeplot = reactive({
+    if(input$time == "monthoutput") {
+    ggplot(monthst,aes(x=factor(month),y=RequestType,fill=count))+
+      geom_tile()+
+      scale_fill_gradient(low="white",high="black")+
+      theme_classic()+
+      xlab("") +
+      ylab("")+
+      theme_classic()
+    } else{
+    if(input$time == "houroutput") {
       ggplot(hourst,aes(x=RequestType,y=hour,fill=count))+
         geom_tile()+
         scale_fill_gradient(low="white",high="black") +
@@ -49,14 +75,20 @@ server = function(input, output) {
         xlab("")+
         ylab("Hour")+
         coord_flip()
-    })
-    
+    } }
+  })
+
     output$requesttime <- renderPlot({
-      monthoutput()
-      houroutput()
+        requesttimeplot()
     })
   
+    weekdayst = reactive({
+      weekdayst = read.csv("weekdayst.csv")
+    })
+    
   output$weekdayst <- renderPlot({
+    weekdayst() = weekdayst() %>%
+    factor(weekdayst()$day, levels = c(2,6,7,5,1,3,4))
     ggplot(weekdayst(),aes(x=day,y=RequestType,fill=count))+
       geom_tile()+
       scale_fill_gradient(low="white",high="black")+
--- ui.R
+++ ui.R
@@ -1,26 +1,30 @@
 
 library(shiny)
 
-library(shiny)
-
 ui = fluidPage(title = "Final Project",
       tabsetPanel(
           tabPanel(title = "Time", 
               navlistPanel(
-                  tabPanel(title = "Hour vs. Day of Week",
-                            plotOutput("dayhour"),
-                            actionButton("renorm", "Resample")
-                            ),
+                  tabPanel(title = "Volume Overtime",
+                      selectInput("vtselect", label = "Service Volume in Different Time Period",
+                          choices = list("Hour vs. Day of Week" = "hourday", 
+                                        "Hour vs. Month" = "hourmonth", 
+                                        "Day of Week vs. Month" = "daymonth"),
+                          selected = "hourday"),
+                      plotOutput("volumetime")
+                      ),
                   
                   tabPanel(title = "Hour vs. Month",
                             plotOutput("monthhr")
                             ),
                              
                   tabPanel(title = "Request Type vs. Time",
-                            plotOutput("requesttime"),
-                            actionButton("monthclick", "Month"),
-                            actionButton("weekdayclick", "Day of Week"),
-                            actionButton("hourclick", "Hour")
+                           selectInput("time", label = "Request Type vs. Time",
+                            choices = list("Month" = "monthoutput", 
+                                           "Day of Week" = "weekdayoutput", 
+                                           "Hour" = "houroutput"),
+                            selected = "monthoutput"),
+                           plotOutput("requesttime")
                             ),
                              
                   tabPanel(title = "Request Type vs. Day of Week",
@@ -30,6 +34,7 @@ ui = fluidPage(title = "Final Project",
                   tabPanel(title = "Request Type vs. Hour",
                             plotOutput("hourst")
                             ))),
+          
             tabPanel(title = "Geographical Area",
                navlistPanel(
                    tabPanel(title = "Council District",
